3b440f4e157ca62693cf5545aec5e759365ef278
Kaustubh-404 <kaustubhp2003@gmail.com>
2025-03-06 22:47:18 +0530

    completing the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Navbar } from "./components/Navbar";
+import Markets from "./pages/Markets";
+import Profile from "./pages/Profile";
+import Create from "./pages/Create";
 import Login from "./pages/Login";
-
+import Bet from "./pages/Bet";
 import { useEffect } from "react";
-
+import Positions from "./pages/Positions";
 import { useAccount } from 'wagmi'
 function App() {
 
@@ -25,6 +28,11 @@ const currentAccount= account.address
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/markets" element={<Markets />} />
+            <Route path="/Positions" element={<Positions />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/bet/:id" element={<Bet />} />
           </Routes>
         </BrowserRouter>
       </main>
